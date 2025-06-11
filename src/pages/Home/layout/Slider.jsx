78be89a6ee336e3e5d0baf8e5493b8d8d852350e
Barnabas Soren <barnabassoren54@gmail.com>
2025-06-11 16:26:36 +0530

    first commit

---
+++ src/pages/Home/layout/Slider.jsx
@@ -0,0 +1,12 @@
+// import { Carousel } from "@material-tailwind/react";
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+
+export function Slider() {
+  return (
+    <div className="text-center border-2 border-amber-950 h-screen">
+      <h1 className="text-6xl">Slider Area</h1>
+    </div>
+  );
+}
